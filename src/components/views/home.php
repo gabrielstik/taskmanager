c9f832920f7ba74907b83858efe6c8999d0d2036
Gabriel Stik <gabriel.stik@gmail.com>
2018-03-27 18:34:48 +0200

    :lipstick: Added 'add card' button

--- src/components/views/home.php
+++ src/components/views/home.php
@@ -1,8 +1,8 @@
 <section class="wall auto-90"> 
-  <h1>Mon mur</h1>
+  <h1 class="wall--title">Mon mur</h1>
   <div class="lists">
     <div class="list">
-      <h2>Mes tâches</h2>
+      <h2 class="list--title">Mes tâches</h2>
       <div class="task checked">
         <span class="task--checkbox">[<span class="task--status-todo"> </span><span class="task--status-done">x</span>]</span>
         <span class="task--name">Acheter un échiquier</span>
@@ -18,6 +18,9 @@
       <div class="task-add">
         <span class="task-add--plus">[+]</span>
       </div>
+    </div>
+    <div class="list">
+      <h2 class="list--title add">+</h2>
     </div>
   </div>
 </section>
