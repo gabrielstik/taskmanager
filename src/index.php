34f629e173577e23f38d9a30284204de89876590
Gabriel Stik <gabriel.stik@gmail.com>
2018-03-27 17:52:42 +0200

    :art: Connected database

--- src/index.php
+++ src/index.php
@@ -5,3 +5,7 @@ require './config.php';
 include './components/Db.php';
 
 include './components/Session.php';
+
+include './components/partials/header.php';
+include './components/views/home.php';
+include './components/partials/footer.php';
