5037278e0038dbba1c393e98e9d2d26bc7e3784d
Gabriel Stik <gabriel.stik@gmail.com>
2018-04-01 18:13:04 +0200

    :sparkles: Can add task in differents walls

--- src/components/views/home.php
+++ src/components/views/home.php
@@ -8,9 +8,9 @@ $walls = $Db->get_walls('gabe');
 <section class="wall auto-90"> 
   <h1 class="wall--title">Mon mur</h1>
   <div class="lists">
+    <? foreach ($walls as $wall) { $tasks = $Db->get_tasks($wall->wall) ?>
     <div class="list">
-      <? foreach ($walls as $wall) { $tasks = $Db->get_tasks($wall->wall) ?>
-      <h2 class="list--title">Mes tâches</h2>
+      <h2 class="list--title"><?= $wall->wall ?></h2>
       <? foreach ($tasks as $task) { ?>
       <div class="task <? if ($task->is_done) { ?>checked<? } ?>">
         <form class="task-check" action="/" method="post">
@@ -26,13 +26,12 @@ $walls = $Db->get_walls('gabe');
       </div>
       <? } ?>
       <form method="post" action="/" class="task-add">
+        <input type="hidden" name="new-task-related-wall" value="<?= $wall->wall ?>"></input>
         <button type="submit" class="task-add--plus">[+]</button>
-        <input class="task-add--name" name="new-task-name"></input>
+        <input type="text" class="task-add--name" name="new-task-name"></input>
       </form>
     </div>
-    <?
-    }
-    ?>
+    <? } ?>
     <div class="list">
       <h2 class="list--title add">+</h2>
     </div>
