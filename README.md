6faf48f0670c83bd842d463fbda812b93126f983
Gabriel Stik <gabriel.stik@gmail.com>
2018-04-01 23:27:35 +0200

    :memo: Writed doc

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# taskmanager
-School project where we have to create a task manager
+# Tasques
+
+Tasques is a school project where we had to create a task app with PHP and MySQL.
+
+
+
+## Prerequisites
+
+![](https://img.shields.io/badge/yarn->1.1.0-blue.svg)![](https://img.shields.io/badge/mamp->0.0.7-blue.svg)![](https://img.shields.io/badge/php->7.1-blue.svg)
+
+
+
+### Database
+
+- [ ] Open PhpMyAdmin
+- [ ] Create a new database named 'tasques'
+- [ ] Go to SQL and paste db.sql file in the field
+- [ ] Execute
+
+### App
+
+```sh
+# Download node_modules
+yarn install
+
+# Generate prod version
+gulp prod
+```
+
+
+
+Now you can run the app.
+
+
+
+## Features
+
+- You can add, remove & edit tasks
+- Tasks have a custom deadline
+- User profiles
+- Multiple walls
+- Responsive website
+
+
+
+## Test user
+
+**username:** suzuki
+
+**password:** bandit
+
+
+
+
+
+## License
+
+This project is under MIT license.
---
+++ db.sql
@@ -0,0 +1,126 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:8889
+-- Généré le :  Dim 01 avr. 2018 à 23:27
+-- Version du serveur :  5.6.38
+-- Version de PHP :  7.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Base de données :  `tasques`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL,
+  `related_wall` varchar(50) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `is_done` tinyint(1) NOT NULL,
+  `deadline` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `tasks`
+--
+
+INSERT INTO `tasks` (`id`, `related_wall`, `title`, `is_done`, `deadline`) VALUES
+(49, '5', 'Sortir Sudo', 0, 1523222198),
+(50, '6', 'Préparer les partiels', 0, 1523222208),
+(51, '6', 'Corriger les devoirs', 1, 1523222214),
+(54, '6', 'Préparer la semaine API', 0, 1523222293),
+(55, '5', 'Finir mon projet', 1, 1523222318),
+(56, '7', 'Que faire cet été ?', 0, 1523222337),
+(57, '8', 'afezrgetr', 0, 1523222444);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(6, 'suzuki', '$2y$10$BTi/WhbWEOtzTNBwDFe/me3IyvFeoGiWE4P5wEYgS/VU2O3WlAkVq');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `walls`
+--
+
+CREATE TABLE `walls` (
+  `id` int(11) NOT NULL,
+  `wall` varchar(50) NOT NULL,
+  `related_user` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `walls`
+--
+
+INSERT INTO `walls` (`id`, `wall`, `related_user`) VALUES
+(5, 'Perso', 'suzuki'),
+(6, 'HETIC', 'suzuki'),
+(7, 'Vacances', 'suzuki');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `walls`
+--
+ALTER TABLE `walls`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT pour la table `walls`
+--
+ALTER TABLE `walls`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
